1fc5679dfe9a3d184d3dde3f928009c92138c4ad
FaizurFahim <faizurfahim111@gmail.com>
2023-01-20 21:58:16 +0600

    this is my 1st commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\DELL\OneDrive\Desktop\Complain_Management_System\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
